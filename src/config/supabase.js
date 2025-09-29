022828514b22ccc766bb4c8a8a2a05419c0cb871
Wim VanHerle <wim.vanherle@gmail.com>
2025-09-29 16:08:57 +0200

    add packages and supabase connection

---
+++ src/config/supabase.js
@@ -0,0 +1,15 @@
+// src/config/supabase.js
+import { createClient } from '@supabase/supabase-js'
+
+// Haal de environment variables op via Vite/Vue
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+// Initialiseer de Supabase client
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Supabase URL or Anon Key not defined in environment variables.")
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+// We zullen deze client gebruiken in al onze Composables.
